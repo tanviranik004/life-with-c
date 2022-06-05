4416ac2e55c136113ad872b7e6a23cd77a07d70e
tanviranik004 <tanvir.anik.009@gmail.com>
2022-06-06 04:02:38 +0600

    variable

---
+++ variable.c
@@ -0,0 +1,21 @@
+/*#include<stdio.h> is a header file library 
+that let us work with input and output functions,
+such as printf()*/
+
+//variables is container that store values
+
+#include<stdio.h>
+int main()
+    {
+        int number1, number2,sum ; // variables
+
+            number1 =10;
+            number2=20;
+
+            sum=number1+number2;
+
+            printf("sum is %d\n",sum);
+
+                return 0;
+
+    }
