4c6389f736e5b661d1a13d8480371b1acf15805f
tanviranik004 <tanvir.anik.009@gmail.com>
2022-06-18 15:08:47 +0600

    output

---
+++ output.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+//This is program is  how show output in c;
+
+int main()
+{
+    printf("Hello world! \n");
+    printf("Hello Anik \n");
+    int age = 24;
+    printf("age is %d \n", age);
+    float pi=3.1416;
+    printf("pi is %f \n", pi);
+    char star= '*';
+    printf("star is %c \n", star);
+    return 0;
+}
