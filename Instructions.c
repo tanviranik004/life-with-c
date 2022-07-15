38b3e25a928d2c79bb60773f9b422363e52fce6d
tanviranik004 <tanvir.anik.009@gmail.com>
2022-07-16 01:41:05 +0600

    Chapter Two Instructions

---
+++ Instructions.c
@@ -0,0 +1,13 @@
+// Three Types of Instructions:
+
+/* 
+    1.Type Declaration Instructions
+    2.Arithmetic Instructions
+    3.Control Instructions
+
+*/
+#include <stdio.h>
+int main(){
+    printf("%d\n", 5>4 && 7>6);
+    return 0;
+}
