065dddbfe6512d0633e2b2a947c667cb24781b4d
tanviranik004 <tanvir.anik.009@gmail.com>
2022-08-22 02:54:14 +0600

    Chapter 4

--- loopSum.c
+++ loopSum.c
@@ -5,10 +5,15 @@ int main() {
     scanf("%d", &n);
 
     int sum=0;
-    for(int i=1; i<=n; i++){
+    for(int i=1, j=n; i<=n && j>=1; i++,j--){
         sum=sum+i; //sum+=i;
+        printf("%d\n",j);
 
     }
-    printf("%d\n", sum);
+    printf("sum is= %d\n", sum);
+    // for(int i=n; i>=1; i--){
+    //     printf("%d\n", i);
+    // }
+
     return 0;
-}
+}   
