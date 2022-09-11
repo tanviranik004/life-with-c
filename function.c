296ff258036c60644c9550fdd1301dd7770bfd24
tanviranik004 <tanvir.anik.009@gmail.com>
2022-09-11 10:57:54 +0600

    Chapter-5

---
+++ function.c
@@ -0,0 +1,28 @@
+// practice Qs 27
+
+//Write 2 function - one to print "Hello" & second to print "good bye".
+
+#include<stdio.h>
+//Declaration//prototype
+void printHello();
+void printGoodbye();
+
+int main(){
+     printHello();
+
+     printGoodbye();
+
+    return 0;
+
+}
+
+//function definition
+
+void printHello(){
+    printf("Hello....!!\n");
+}
+
+void printGoodbye(){
+    printf("Goodbye.....");
+}
+
